ea397c23364927750d544c1e9d544608138d3df5
Tim Robillard <hello@timrobillard.ca>
2021-07-01 17:00:07 -0400

    add cursive font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css2?family=IM+Fell+English:ital@1&family=Lato:wght@400;700&display=swap" rel="stylesheet"></link>
             </Head>
             <Component {...pageProps} />
             <GlobalStyle />
