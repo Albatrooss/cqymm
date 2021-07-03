35626005c89f478f16cbac4f8d6eb3cf579ef9a4
Tim Robillard <hello@timrobillard.ca>
2021-07-02 21:38:07 -0400

    slideshow updates

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -11,20 +11,19 @@ const Slideshow = ({ imgs, time }: props) => {
     const [slide, setSlide] = useState<number>(0);
 
     const changeSlide = () => {
-        console.log('fn')
         setSlide(prev => (prev + 1) % imgs.length);
     }
 
     useEffect(() => {
-        console.log('effect')
         setTimeout(changeSlide, time)
     }, [slide]);
 
-    let slides = imgs.map((img, i) => <Img src={img} fade={slide === i} />)
+    let slides = imgs.map((img, i) => <Img key={i} src={img} fade={slide === i} />)
 
     return (
         <Wrapper>
             {slides}
+            <Strip />
         </Wrapper>
     )
 
@@ -34,6 +33,15 @@ export default Slideshow;
 
 const Wrapper = styled.div`
     position: relative;
+    width: 100%;
+    max-width: 800px;
+    height: 405px;
+    margin: 0 auto;
+    overflow: hidden;
+    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.2);
+    background-color: #FFF;
+    padding: 12px;
+    border-radisu: 6px;
 `;
 
 interface ImgProps {
@@ -41,10 +49,21 @@ interface ImgProps {
 }
 const Img = styled.img<ImgProps>`
     position: absolute;
-    left: 0;
-    top: 2rem;
-    max-width: 600px;
+    left: 6px;
+    top: 6px;
+    width: calc(100% - 12px);
     opacity: ${({fade}) => fade ? 1 : 0};
 
     transition: opacity 1000ms ease;
+`;
+
+const Strip = styled.div`
+    height: 6px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+    background-color: #FFF;
+    border-radisu: 6px;
 `;
